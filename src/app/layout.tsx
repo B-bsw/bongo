a1406baf0dfc29cd1e25f862d251b815edcfd8e1
BB <sorawitthassadorn15@gmail.com>
2025-10-18 00:52:14 +0700

    fix viewfit over

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata, Viewport } from 'next'
 import './style/app.css'
 
-
 export const metadata: Metadata = {
     title: 'Bongo Bongo',
     description: 'this is app',
 }
 export const viewport: Viewport = {
+    viewportFit: 'cover',
     width: 'device-width',
     initialScale: 1,
     maximumScale: 1,
